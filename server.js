bdbe5b48fc597b736e0de4cad213c854c12138fc
Nezertiam <mawyn.nhek@gmail.com>
2021-10-14 19:47:23 +0200

    [ADD] User authentication

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"))
 
 // Define routes
